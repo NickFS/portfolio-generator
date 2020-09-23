1e0dd531682ef6c92160b3cc32bcc8a5b8e3ef27
NickFS <nickolas.t.foster@gmail.com>
2020-09-22 23:02:36 -0600

    initial files

---
+++ app.js
@@ -0,0 +1,26 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+ 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('================');
+
+const printProfileData = (profileDataArr) => {
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+    }
+printProfileData(profileDataArgs);
